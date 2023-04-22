2ed315e94577557e3ed83e74e650f2f28f3b9107
EricJin2002 <769327841@qq.com>
2023-04-22 09:45:11 +0800

    fix: add resize in LoadProposalsFromAnnotations

--- checkpoints/custom_transforms.py
+++ checkpoints/custom_transforms.py
@@ -9,6 +9,8 @@ class LoadProposalsFromAnnotations(BaseTransform):
         pass
 
     def transform(self, results: dict) -> dict:
+        # print("Enter LoadProposalsFromAnnotations.transform")
+        # print("results:", results)
         assert 'gt_bboxes' in results, 'results must contain key: gt_bboxes'
         # print('results[\'gt_bboxes\']:', results['gt_bboxes'])
         if (not isinstance(results['gt_bboxes'], (Tensor, np.ndarray))):
@@ -16,5 +18,19 @@ class LoadProposalsFromAnnotations(BaseTransform):
             results['proposals'] = results['gt_bboxes'].tensor
         else:
             results['proposals'] = results['gt_bboxes']
-        results['proposals_scores'] = np.full(len(results['gt_bboxes']), 0.99, dtype=np.float32)
+
+        # results['proposals'] = np.array([[22, 95, 22+256, 95+108]], dtype=np.float32)
+
+        # resize proposals
+        factor = np.array([
+            results['scale_factor'][0], results['scale_factor'][1],
+            results['scale_factor'][0], results['scale_factor'][1]
+        ], dtype=np.float32)
+
+        results['proposals'] = np.multiply(results['proposals'], factor)
+        results['proposals_scores'] = np.full(len(results['proposals']), 0.99, dtype=np.float32)
+        # print(results['proposals'])
+
+        # results['proposals'] = np.array([[385.3159, 335.9352, 860.2880, 719.0645],], dtype=np.float32)
+        # results['proposals_scores'] = np.full(1, 0.99, dtype=np.float32)
         return results
